f803f934f7fdcc6455366244a3928125674b470b
Ramo0510 <omardij01@gmail.com>
2024-02-23 22:02:22 -0500

    Comp updated

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -26,14 +26,19 @@ const ExpenseTotal = () => {
             </div>
 
             <div className="currency-dropdown" style={{ width: '250px', height: '60px', fontSize: '16px', backgroundColor: '#f8d7da', color: '#721c24', marginLeft: '10px', borderRadius: '6px', paddingTop: '14px'}}>
-            
-            <label style={{ marginLeft: '1rem' }}>Currency</label>
-            <select value={selectedCurrency} onChange={handleCurrencyChange} style={{ marginLeft: '0.5rem', fontSize: '16px', padding: '8px', backgroundColor: '#f8d7da', color: 'white' }}>
-                <option value="$">Dollar</option>
-                <option value="£">Pound</option>
-                <option value="€">Euro</option>
-                <option value="₹">Rupee</option>
-            </select>
+
+            <label style={{ marginLeft: '1rem'}}>Currency {selectedCurrency}</label>
+
+            <div className="styled-select" style={{ position: 'relative', marginTop: '-23px', marginLeft: '6rem', overflow: 'hidden', backgroundColor: '#f8d7da', color: 'white', borderRadius: '6px' }}>
+            <span style={{color:'#f8d7da'}}>{selectedCurrency}</span>
+        <select value={selectedCurrency} onChange={handleCurrencyChange} style={{ position: 'absolute', top: 0, left: '0px', width: '100px', height: '100%', opacity: 1, cursor: 'pointer', fontSize: '16px', padding: '8px', backgroundColor: 'transparent', color: 'black', border:'none' }}>
+          <option value="$">$ Dollar</option>
+          <option value="£">£ Pound</option>
+          <option value="€">€ Euro</option>
+          <option value="₹">₹ Rupee</option>
+        </select>
+
+      </div>
             
             </div>
             
